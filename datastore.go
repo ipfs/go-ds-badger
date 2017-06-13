0da36eadc8afa51aac99095c56c965171430caf3
Łukasz Magiera <magik6k@gmail.com>
2017-06-13 19:51:53 +0200

    Implement basic methods

--- datastore.go
+++ datastore.go
@@ -2,9 +2,9 @@ package badger
 
 import (
 	badger "github.com/dgraph-io/badger/badger"
-	ds "github.com/ipfs/go-datastore"
-	dsq "github.com/ipfs/go-datastore/query"
 	"github.com/pkg/errors"
+	ds "gx/ipfs/QmRWDav6mzWseLWeYfVd5fvUKiVe9xNH29YfMF438fG364/go-datastore"
+	dsq "gx/ipfs/QmRWDav6mzWseLWeYfVd5fvUKiVe9xNH29YfMF438fG364/go-datastore/query"
 )
 
 type datastore struct {
@@ -28,19 +28,38 @@ func NewDatastore(path string) (*datastore, error) {
 }
 
 func (d *datastore) Put(key ds.Key, value interface{}) (err error) {
-	return errors.New("not implemented")
+	val, ok := value.([]byte)
+	if !ok {
+		return ds.ErrInvalidType
+	}
+
+	return d.DB.Set(key.Bytes(), val)
 }
 
 func (d *datastore) Get(key ds.Key) (value interface{}, err error) {
-	return nil, errors.New("not implemented")
+	var item badger.KVItem
+	err = d.DB.Get(key.Bytes(), &item)
+	if err != nil {
+		return nil, err
+	}
+	if item.Value() == nil {
+		return nil, ds.ErrNotFound
+	}
+	return item.Value(), nil
 }
 
 func (d *datastore) Has(key ds.Key) (exists bool, err error) {
-	return nil, errors.New("not implemented")
+	var item badger.KVItem
+	err = d.DB.Get(key.Bytes(), &item)
+	if err != nil {
+		return false, err
+	}
+
+	return item.Value() != nil, nil
 }
 
 func (d *datastore) Delete(key ds.Key) (err error) {
-	return errors.New("not implemented")
+	return d.DB.Delete(key.Bytes())
 }
 
 func (d *datastore) Query(q dsq.Query) (dsq.Results, error) {
@@ -58,6 +77,8 @@ func (d *datastore) QueryOrig(q dsq.Query) (dsq.Results, error) {
 func (d *datastore) Close() (err error) {
 	return d.DB.Close()
 }
+
+func (d *datastore) IsThreadSafe() {}
 
 func (d *datastore) Batch() (ds.Batch, error) {
 	return nil, errors.New("not implemented")
