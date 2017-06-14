1ef242ff0ce7379d3f2b67aad5a039bc673ca081
Łukasz Magiera <magik6k@gmail.com>
2017-06-14 14:56:55 +0200

    Add more tests

--- ds_test.go
+++ ds_test.go
@@ -7,6 +7,10 @@ import (
 
 	ds "github.com/ipfs/go-datastore"
 	dsq "github.com/ipfs/go-datastore/query"
+	"bytes"
+	"sort"
+	"fmt"
+	"math/rand"
 )
 
 var testcases = map[string]string{
@@ -45,6 +49,14 @@ func addTestCases(t *testing.T, d *datastore, testcases map[string]string) {
 	for k, v := range testcases {
 		dsk := ds.NewKey(k)
 		if err := d.Put(dsk, []byte(v)); err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	err := d.Put(ds.NewKey("/foo"),  nil)
+	if err != ds.ErrInvalidType {
+		t.Error("Expected err to be ds.ErrInvalidType")
+		if err != nil {
 			t.Fatal(err)
 		}
 	}
@@ -117,6 +129,33 @@ func TestHas(t *testing.T) {
 	}
 }
 
+func TestNotExistGet(t *testing.T) {
+	d, done := newDS(t)
+	defer done()
+	addTestCases(t, d, testcases)
+
+	has, err := d.Has(ds.NewKey("/a/b/c/d"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	if has {
+		t.Error("Key should not be found")
+	}
+
+	val, err := d.Get(ds.NewKey("/a/b/c/d"))
+	if val != nil {
+		t.Error("Key should not be found")
+	}
+
+	if err != ds.ErrNotFound {
+		t.Error("Error was not set to ds.ErrNotFound")
+		if err != nil {
+			t.Error(err)
+		}
+	}
+}
+
 func TestDelete(t *testing.T) {
 	d, done := newDS(t)
 	defer done()
@@ -182,6 +221,14 @@ func TestBatching(t *testing.T) {
 		}
 	}
 
+	err = b.Put(ds.NewKey("/foo"),  nil)
+	if err != ds.ErrInvalidType {
+		t.Error("Expected err to be ds.ErrInvalidType")
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
 	err = b.Commit()
 	if err != nil {
 		t.Fatal(err)
@@ -197,4 +244,214 @@ func TestBatching(t *testing.T) {
 			t.Fatal("got wrong data!")
 		}
 	}
+
+	//Test delete
+
+	b, err = d.Batch()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = b.Delete(ds.NewKey("/a/b"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = b.Delete(ds.NewKey("/a/b/c"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = b.Commit()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rs, err := d.Query(dsq.Query{Prefix: "/"})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expectMatches(t, []string{
+		"/a",
+		"/a/b/d",
+		"/a/c",
+		"/a/d",
+		"/e",
+		"/f",
+	}, rs)
+
+
 }
+
+// Tests from basic_tests from go-datastore
+
+func TestBasicPutGet(t *testing.T) {
+	d, done := newDS(t)
+	defer done()
+
+	k := ds.NewKey("foo")
+	val := []byte("Hello Datastore!")
+
+	err := d.Put(k, val)
+	if err != nil {
+		t.Fatal("error putting to datastore: ", err)
+	}
+
+	have, err := d.Has(k)
+	if err != nil {
+		t.Fatal("error calling has on key we just put: ", err)
+	}
+
+	if !have {
+		t.Fatal("should have key foo, has returned false")
+	}
+
+	out, err := d.Get(k)
+	if err != nil {
+		t.Fatal("error getting value after put: ", err)
+	}
+
+	outb, ok := out.([]byte)
+	if !ok {
+		t.Fatalf("output type wasnt []byte, it was %T", out)
+	}
+
+	if !bytes.Equal(outb, val) {
+		t.Fatal("value received on get wasnt what we expected:", outb)
+	}
+
+	have, err = d.Has(k)
+	if err != nil {
+		t.Fatal("error calling has after get: ", err)
+	}
+
+	if !have {
+		t.Fatal("should have key foo, has returned false")
+	}
+
+	err = d.Delete(k)
+	if err != nil {
+		t.Fatal("error calling delete: ", err)
+	}
+
+	have, err = d.Has(k)
+	if err != nil {
+		t.Fatal("error calling has after delete: ", err)
+	}
+
+	if have {
+		t.Fatal("should not have key foo, has returned true")
+	}
+}
+
+func SubtestNotFounds(t *testing.T) {
+	d, done := newDS(t)
+	defer done()
+
+	badk := ds.NewKey("notreal")
+
+	val, err := d.Get(badk)
+	if err != ds.ErrNotFound {
+		t.Fatal("expected ErrNotFound for key that doesnt exist, got: ", err)
+	}
+
+	if val != nil {
+		t.Fatal("get should always return nil for not found values")
+	}
+
+	have, err := d.Has(badk)
+	if err != nil {
+		t.Fatal("error calling has on not found key: ", err)
+	}
+	if have {
+		t.Fatal("has returned true for key we don't have")
+	}
+}
+
+func TestManyKeysAndQuery(t *testing.T) {
+	d, done := newDS(t)
+	defer done()
+
+	var keys []ds.Key
+	var keystrs []string
+	var values [][]byte
+	count := 100
+	for i := 0; i < count; i++ {
+		s := fmt.Sprintf("%dkey%d", i, i)
+		dsk := ds.NewKey(s)
+		keystrs = append(keystrs, dsk.String())
+		keys = append(keys, dsk)
+		buf := make([]byte, 64)
+		rand.Read(buf)
+		values = append(values, buf)
+	}
+
+	t.Logf("putting %d values", count)
+	for i, k := range keys {
+		err := d.Put(k, values[i])
+		if err != nil {
+			t.Fatalf("error on put[%d]: %s", i, err)
+		}
+	}
+
+	t.Log("getting values back")
+	for i, k := range keys {
+		val, err := d.Get(k)
+		if err != nil {
+			t.Fatalf("error on get[%d]: %s", i, err)
+		}
+
+		valb, ok := val.([]byte)
+		if !ok {
+			t.Fatalf("expected []byte as output from get, got: %T", val)
+		}
+
+		if !bytes.Equal(valb, values[i]) {
+			t.Fatal("input value didnt match the one returned from Get")
+		}
+	}
+
+	t.Log("querying values")
+	q := dsq.Query{KeysOnly: true}
+	resp, err := d.Query(q)
+	if err != nil {
+		t.Fatal("calling query: ", err)
+	}
+
+	t.Log("aggregating query results")
+	var outkeys []string
+	for {
+		res, ok := resp.NextSync()
+		if res.Error != nil {
+			t.Fatal("query result error: ", res.Error)
+		}
+		if !ok {
+			break
+		}
+
+		outkeys = append(outkeys, res.Key)
+	}
+
+	t.Log("verifying query output")
+	sort.Strings(keystrs)
+	sort.Strings(outkeys)
+
+	if len(keystrs) != len(outkeys) {
+		t.Fatal("got wrong number of keys back")
+	}
+
+	for i, s := range keystrs {
+		if outkeys[i] != s {
+			t.Fatal("in key output, got %s but expected %s", outkeys[i], s)
+		}
+	}
+
+	t.Log("deleting all keys")
+	for _, k := range keys {
+		if err := d.Delete(k); err != nil {
+			t.Fatal(err)
+		}
+	}
+}
+
